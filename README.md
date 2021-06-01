47c95c114300db7d6efa2ccfa6646a01416003f6
Jeremias Pettinen <hi@jeremi.as>
2021-06-01 17:37:40 +0000

    Add typings

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Sanitizes Twitch chatlogs (as obtained from Chatterino) by removing uninterestin
 ## Usage
 ```process-twitch-log <file> [<file>...]```
 
-Data is read from files called `bots`, `commands` and `emotes`, which contain one item per line (see example files). By default these files are searched for in the current working directory, but you can specify another directory with the `PROCESSTWITCHLOG_DIR` environment variable.
+Data is read from files called `bots`, `commands` and `emotes`, which contain one item per line (see example files). By default these files are searched for in the current working directory, but you can specify another directory with the `PROCESS_TWITCH_LOG_DIR` environment variable.
--- process_twitch_log/__init__.py
+++ process_twitch_log/__init__.py
@@ -2,58 +2,63 @@ import fileinput
 import os
 import re
 from pathlib import Path
+from typing import Iterable, Union
 
 
-def keep_line(line, data):
+def keep_line(
+    line: str,
+    message_re: str,
+    skip_patterns: Iterable[str],
+    commands: Iterable[str],
+    emotes: Iterable[str],
+) -> bool:
     # Skip sub notifications and bots' messages
-    for pattern in data['skip_patterns']:
+    for pattern in skip_patterns:
         if re.match(pattern, line):
             return False
 
-    msg_match = re.match(data['message_re'], line)
-    if msg_match:
+    if msg_match := re.match(message_re, line):
         msg = msg_match[2]
         # Skip common commands
-        if msg in data['commands']:
+        if msg in commands:
             return False
         # Skip messages consisting of emotes only
         words = msg.split()
-        if all(word in data['emotes'] for word in words):
+        if all(word in emotes for word in words):
             return False
     return True
 
 
-def main():
-    date_re = r'\[\d\d:\d\d:\d\d\]'
-    data = {
-        'message_re': rf'{date_re}\s+([^:]+):\s+(.*)'
-    }
+def main() -> None:
+    date_re = r"\[\d\d:\d\d:\d\d\]"
+    message_re = rf"{date_re}\s+([^:]+):\s+(.*)"
 
-    data_dir = os.getenv('PROCESSTWITCHLOG_DIR', '.')
-    for filename in ['bots', 'commands', 'emotes']:
+    data = {}
+    data_dir = os.getenv("PROCESS_TWITCH_LOG_DIR", ".")
+    for filename in ["bots", "commands", "emotes"]:
         path = Path(data_dir, filename)
         try:
-            with open(path) as f:
-                data[filename] = list(map(lambda s: s.strip(), f))
+            with open(path) as file:
+                data[filename] = {line.strip() for line in file}
         except FileNotFoundError:
-            data[filename] = []
+            data[filename] = set()
 
     notifications = [
-        r'is gifting \d+ Tier',
-        r'gifted a Tier \d+ sub to',
-        'is continuing the Gift Sub',
-        'is paying forward the Gift they god',
-        'subscribed at Tier',
-        'subscribed with Twitch Prime',
-        'converted from a Twitch Prime sub'
+        r"is gifting \d+ Tier",
+        r"gifted a Tier \d+ sub to",
+        "is continuing the Gift Sub",
+        "is paying forward the Gift they got",
+        "subscribed at Tier",
+        "subscribed with Twitch Prime",
+        "converted from a Twitch Prime sub",
     ]
-    data['skip_patterns'] = list(map(
-        lambda notification: rf'{date_re} \S.* {notification}',
-        notifications))
+    skip_patterns = {
+        rf"{date_re} \S.* {notification}" for notification in notifications
+    }
 
-    for bot in data['bots']:
-        data['skip_patterns'].append(rf'{date_re}  {bot}:')
+    for bot in data["bots"]:
+        skip_patterns.add(rf"{date_re}  {bot}:")
 
     for line in fileinput.input():
-        if keep_line(line, data):
-            print(line, end='')
+        if keep_line(line, message_re, skip_patterns, data["commands"], data["emotes"]):
+            print(line, end="")
